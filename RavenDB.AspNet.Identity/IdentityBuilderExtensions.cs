14e4b6a3a55f09abc78aba944a4ba9da6dd7a448
Joe Kriefall <joe.kriefall@ilmservice.com>
2015-07-13 15:07:14 -0500

    Removed extraneous using statements

--- RavenDB.AspNet.Identity/IdentityBuilderExtensions.cs
+++ RavenDB.AspNet.Identity/IdentityBuilderExtensions.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.Framework.DependencyInjection;
-using IServiceCollection = Microsoft.Framework.DependencyInjection.IServiceCollection;
 
 namespace RavenDB.AspNet.Identity
 {
